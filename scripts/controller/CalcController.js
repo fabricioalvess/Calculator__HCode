a992705b635581ff4c4c9abc6023c56e1071a80b
Fabrício Cintra <fabricioalvescintra@gmail.com>
2023-02-23 12:58:56 -0300

    First Commit

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,57 @@
+class CalcController{
+
+
+    constructor(){
+        this._locale = 'pt-BR';
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this._currentDate; 
+        this.initialize();
+
+    }
+
+    initialize(){
+         setInterval(()=>{
+            this.displayDate = this._currentDate.toLocaleDateString(this._locale);
+            this.displayTime = this._currentDate.toLocaleTimeString(this._locale);
+         },1000);
+    }
+
+
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }
+    set displayTime(value){
+        return this._timeEl.innerHTML = value;
+    }
+
+
+    get displayDate(){
+        return this._dateEl.innerHTML;
+    }
+    set displayDate(value){
+        return this._dateEl.innerHTML = value;
+    }
+
+
+    get displayCalc(){
+        return this._displayCalcEl.innerHTML;
+    }
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value;
+    }
+
+
+    get _currentDate(){
+        return new Date();
+    }
+
+    set _currentDate(value){
+        this.__currentDate = value;
+    }
+
+
+
+
+}
